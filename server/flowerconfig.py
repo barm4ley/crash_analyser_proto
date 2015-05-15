ecf8c9b980401bf7ff213ab3f6eca506c239b88c
barm4ley <mamu@Totenkopf.local>
2015-05-15 17:19:43 +0300

    Added config for flower

---
+++ server/flowerconfig.py
@@ -0,0 +1,9 @@
+
+# Broker settings
+BROKER_URL = "amqp://guest:guest@172.19.8.101:5672//"
+
+# RabbitMQ management api
+broker_api = 'http://guest:guest@172.19.8.101:8080/api/'
+
+# Enable debug logging
+#logging = 'INFO'
