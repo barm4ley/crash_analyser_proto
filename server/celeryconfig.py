8380e0e74eae119730aa4cde7d912ef199a0209c
barm4ley <mamu@Totenkopf.local>
2015-05-15 17:23:18 +0300

    Added comments to celery config file

--- server/celeryconfig.py
+++ server/celeryconfig.py
@@ -1,15 +1,29 @@
 
-SECRET_KEY = "top_secret"
+#--- RESULT BACKEND SETTINGS ---#
 
-CELERY_RESULT_BACKEND = "mongodb://172.19.8.101:27017/"
+# RabbitMQ
 #CELERY_RESULT_BACKEND = 'amqp'
-CELERY_MONGODB_BACKEND_SETTINGS = {
-        'database': 'celerydb',
-        'taskmeta_collection': 'taskmeta_collection',
-}
 
+# Redis
+CELERY_RESULT_BACKEND = 'redis://172.19.8.102:6379/0'
+
+# MongoDB
+#CELERY_RESULT_BACKEND = "mongodb://172.19.8.101:27017/"
+#CELERY_MONGODB_BACKEND_SETTINGS = {
+        #'database': 'celerydb',
+        #'taskmeta_collection': 'taskmeta_collection',
+#}
+
+
+#--- MESSAGE BROKER SETTINGS ---#
 BROKER_URL = "amqp://guest:guest@172.19.8.101:5672//"
+
+
+#--- MODULES TO USE ---#
 CELERY_IMPORTS = ("tasks",)
+
+
+#--- EXCHANGE DATA FORMAT ---#
 
 #CELERY_TASK_SERIALIZER = 'json'
 #CELERY_RESULT_SERIALIZER = 'json'
@@ -20,6 +34,9 @@ CELERY_ACCEPT_CONTENT=['pickle']
 CELERY_TASK_SERIALIZER = 'pickle'
 CELERY_RESULT_SERIALIZER = CELERY_TASK_SERIALIZER
 
+#--- OTHER OPTIONS ---#
 CELERY_DISABLE_RATE_LIMITS = True
+
+
 
 DB_URL = "mongodb://172.19.8.101:27017"
