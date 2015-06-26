027c8cf4864960e640f0b6e06cb9c989a9770234
barm4ley <mamu@Totenkopf.local>
2015-06-26 14:00:48 +0300

    Added fuctionality to get celery configuration from environment variables.

--- server/celeryconfig.py
+++ server/celeryconfig.py
@@ -1,34 +1,30 @@
+from os import getenv
+from datetime import timedelta
 
 #--- RESULT BACKEND SETTINGS ---#
 
-# RabbitMQ
-#CELERY_RESULT_BACKEND = 'amqp'
-
 # Redis
-CELERY_RESULT_BACKEND = 'redis://172.19.8.101:6379/0'
-
-# MongoDB
-#CELERY_RESULT_BACKEND = "mongodb://172.19.8.101:27017/"
-#CELERY_MONGODB_BACKEND_SETTINGS = {
-        #'database': 'celerydb',
-        #'taskmeta_collection': 'taskmeta_collection',
-#}
-
+RESULT_BACKEND_ADDR = getenv('RESULT_BACKEND_ADDR', 'localhost')
+RESULT_BACKEND_PORT = getenv('RESULT_BACKEND_PORT', 6379)
+CELERY_RESULT_BACKEND = 'redis://{url}:{port}/0'.format(url=RESULT_BACKEND_ADDR, port=RESULT_BACKEND_PORT)
 
 #--- MESSAGE BROKER SETTINGS ---#
-BROKER_URL = "amqp://guest:guest@172.19.8.101:5672//"
-
+#BROKER_URL = "amqp://guest:guest@10.0.2.15:5672//"
+MESSAGE_BROKER_ADDR = getenv('MESSAGE_BROKER_ADDR', 'localhost')
+MESSAGE_BROKER_PORT = getenv('MESSAGE_BROKER_PORT', 5672)
+MESSAGE_BROKER_USER = getenv('MESSAGE_BROKER_USER', 'admin')
+MESSAGE_BROKER_PASS = getenv('MESSAGE_BROKER_PASS', 'admin')
+BROKER_URL = "amqp://{user}:{password}@{url}:{port}//".format(
+        user=MESSAGE_BROKER_USER,
+        password=MESSAGE_BROKER_PASS,
+        url=MESSAGE_BROKER_ADDR,
+        port=MESSAGE_BROKER_PORT)
 
 #--- MODULES TO USE ---#
 CELERY_IMPORTS = ("tasks",)
 
 
 #--- EXCHANGE DATA FORMAT ---#
-
-#CELERY_TASK_SERIALIZER = 'json'
-#CELERY_RESULT_SERIALIZER = 'json'
-#CELERY_RESULT_SERIALIZER = 'msgpack'
-#CELERY_ACCEPT_CONTENT=['json', 'msgpack']
 
 CELERY_ACCEPT_CONTENT=['pickle']
 CELERY_TASK_SERIALIZER = 'pickle'
@@ -38,7 +34,6 @@ CELERY_RESULT_SERIALIZER = CELERY_TASK_SERIALIZER
 CELERY_DISABLE_RATE_LIMITS = True
 
 #---------------------------------------------------------------#
-from datetime import timedelta
 
 CELERYBEAT_SCHEDULE = {
     'do-each-10-sec': {
@@ -51,4 +46,11 @@ CELERY_TIMEZONE = 'UTC'
 #---------------------------------------------------------------#
 
 
-DB_URL = "mongodb://172.19.8.101:27017"
+STORAGE_ADDR = getenv('STORAGE_ADDR', 'localhost')
+STORAGE_PORT = getenv('STORAGE_PORT', 27017)
+DB_URL = "mongodb://{url}:{port}".format(url=STORAGE_ADDR, port=STORAGE_PORT)
+
+
+print("CELERY_RESULT_BACKEND: {}".format(CELERY_RESULT_BACKEND))
+print("BROKER_URL: {}".format(BROKER_URL))
+print("DB_URL: {}".format(DB_URL))
